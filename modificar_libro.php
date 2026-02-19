e11bbea014b2232045711c0aed3d46f20e43e64d
Javier Mas Díaz <jmasdiaz3.0@gmail.com>
2026-02-19 16:40:40 +0100

    Update database connection credentials in modificar_libro.php

--- modificar_libro.php
+++ modificar_libro.php
@@ -2,7 +2,7 @@
 	if (!isset($_REQUEST['titulo']))
 		header("Location:listado_libros.php");
 
-	$bd = @mysqli_connect("db", "root", "", "biblioteca", 3306);
+	$bd = @mysqli_connect("db", "root", "rootpass", "biblioteca", 3306);
 	mysqli_set_charset($bd, "utf8");
 	if (mysqli_connect_errno() != 0)
 	{
@@ -40,4 +40,5 @@
 		mysqli_close($bd);
 	}
 ?>
+
 
