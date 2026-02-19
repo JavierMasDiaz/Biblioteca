549a74c3c2d38698b2808d9570809a96c8eb3f34
Javier Mas Díaz <jmasdiaz3.0@gmail.com>
2026-02-19 16:36:27 +0100

    Change database connection parameters in borrar_libro.php

--- borrar_libro.php
+++ borrar_libro.php
@@ -2,7 +2,7 @@
 	if (!isset($_REQUEST['id']))
 		header("Location:listado_libros.php");
 
-	$bd = @mysqli_connect("localhost", "root", "", "biblioteca", 3306);
+	$bd = @mysqli_connect("db", "root", "", "biblioteca", 3306);
 	mysqli_set_charset($bd, "utf8");
 	if (mysqli_connect_errno() != 0)
 	{
@@ -24,4 +24,5 @@
 		mysqli_close($bd);
 	}
 ?>
+
 
